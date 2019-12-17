1ed6c501a4e1175c59287fa65e4513ab0f10b3ce
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-12-17 21:39:11 +0900

    modify template

--- templates/index.js
+++ templates/index.js
@@ -1,19 +1,20 @@
+'use strict';
 import * as fs from 'fs';
 import 'core-js';
 // import bigInt from 'big-integer'
 
 // inputに入力データ全体が入る
-function Main(input) {
+function main(input) {
   // 1行目がinput[0], 2行目がinput[1], …に入る
-  input = input.split('\n');
-  tmp = input[1].split(' ');
+  // input[0]に' 'で分けられてる
+  const inputs = input.split('\n')[0].split(' ');
+  console.log(inputs);
   //文字列から10進数に変換するときはparseIntを使います
-  var a = parseInt(input[0], 10);
-  var b = parseInt(tmp[0], 10);
-  var c = parseInt(tmp[1], 10);
-  var s = input[2];
+  const a1 = parseInt(inputs[0], 10);
+  const a2 = parseInt(inputs[1], 10);
+  const a3 = parseInt(inputs[2], 10);
   //出力
-  console.log('%d %s', a + b + c, s);
+  console.log(a1, a2, a3);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
