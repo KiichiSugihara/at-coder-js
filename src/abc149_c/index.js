d0c22321b87804511f67655563e5ef2fd58df98c
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-12-30 23:07:30 +0900

    submit : abc149_c

--- src/abc149_c/index.js
+++ src/abc149_c/index.js
@@ -3,18 +3,45 @@ import * as fs from 'fs';
 import 'core-js';
 // import bigInt from 'big-integer'
 
+function primeCheck(num) {
+  //2 は素数なので true を返す
+  if (num === 2) {
+    return true;
+  } else {
+    for (let i = 2; i < num; i++) {
+      //2以上の数で割ったとき余りが0になれば false を返す。つまり素数ではない。
+      if (num % i === 0) {
+        return false;
+        break;
+      }
+
+      //ループが最後まで行く、つまり割れる数がなかったら true を返す。つまり素数。
+      if (i + 1 === num) {
+        return true;
+      }
+    }
+  }
+}
+function nextPrime(startNumber) {
+  let nextNumber = 0;
+  let counter = startNumber;
+  for (counter; counter < 1000000; counter++) {
+    if (primeCheck(counter)) {
+      nextNumber = counter;
+      break;
+    }
+  }
+  return nextNumber;
+}
+
 // inputに入力データ全体が入る
 function main(input) {
   // 1行目がinput[0], 2行目がinput[1], …に入る
   // input[0]に' 'で分けられてる
   const inputs = input.split('\n')[0].split(' ');
-  console.log(inputs);
   //文字列から10進数に変換するときはparseIntを使います
   const a1 = parseInt(inputs[0], 10);
-  const a2 = parseInt(inputs[1], 10);
-  const a3 = parseInt(inputs[2], 10);
-  //出力
-  console.log(a1, a2, a3);
+  console.log(nextPrime(a1));
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
