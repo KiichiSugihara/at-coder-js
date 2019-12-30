2c058b86d376caedb2eeb15f1b59f7718a8044f9
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-12-30 22:40:10 +0900

    submit : abc149_b

--- src/abc149_b/index.js
+++ src/abc149_b/index.js
@@ -3,18 +3,27 @@ import * as fs from 'fs';
 import 'core-js';
 // import bigInt from 'big-integer'
 
+function cookieCheck(cookieA, cookieB, actionK) {
+  const cookieSum = cookieA + cookieB;
+  if (cookieSum <= actionK) {
+    console.log(0, 0);
+  } else if (cookieA <= actionK && actionK < cookieSum) {
+    console.log(0, cookieSum - actionK);
+  } else {
+    console.log(cookieA - actionK, cookieB);
+  }
+}
+
 // inputに入力データ全体が入る
 function main(input) {
   // 1行目がinput[0], 2行目がinput[1], …に入る
   // input[0]に' 'で分けられてる
   const inputs = input.split('\n')[0].split(' ');
-  console.log(inputs);
   //文字列から10進数に変換するときはparseIntを使います
-  const a1 = parseInt(inputs[0], 10);
-  const a2 = parseInt(inputs[1], 10);
-  const a3 = parseInt(inputs[2], 10);
-  //出力
-  console.log(a1, a2, a3);
+  const cookieA = parseInt(inputs[0], 10);
+  const cookieB = parseInt(inputs[1], 10);
+  const actionK = parseInt(inputs[2], 10);
+  cookieCheck(cookieA, cookieB, actionK);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
