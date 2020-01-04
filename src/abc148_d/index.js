da8ee64d87d027913a50454edd36756268dfc881
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-04 19:35:14 +0900

    retire: 1bc148_d

--- src/abc148_d/index.js
+++ src/abc148_d/index.js
@@ -3,18 +3,37 @@ import * as fs from 'fs';
 import 'core-js';
 // import bigInt from 'big-integer'
 
+function crashBrocks(length, blocks) {
+  let sortedBlocks = blocks;
+  sortedBlocks.sort(function(a, b) {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+  });
+  let blockFinal = 0;
+  for (let i = 0; i < length; i++) {
+    if (Number(sortedBlocks[i]) === blockFinal + 1) {
+      blockFinal = i + 1;
+    } else if (Number(sortedBlocks[i]) === blockFinal) {
+      continue;
+    } else {
+      break;
+    }
+  }
+  // 処理: 元々の順番で思考
+  console.log(blocks, sortedBlocks, blockFinal);
+  // return length, blocks;
+}
 // inputに入力データ全体が入る
 function main(input) {
   // 1行目がinput[0], 2行目がinput[1], …に入る
   // input[0]に' 'で分けられてる
-  const inputs = input.split('\n')[0].split(' ');
-  console.log(inputs);
+  const length = input.split('\n')[0].split(' ');
   //文字列から10進数に変換するときはparseIntを使います
-  const a1 = parseInt(inputs[0], 10);
-  const a2 = parseInt(inputs[1], 10);
-  const a3 = parseInt(inputs[2], 10);
+  const blocks = input.split('\n')[1].split(' ');
   //出力
-  console.log(a1, a2, a3);
+  // console.log(crashBrocks(length, blocks));
+  crashBrocks(length, blocks);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
