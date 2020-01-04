4d3a717ac037bf2f2b20a13e055672d0cbc57a7a
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-04 18:38:49 +0900

    finish: abc148_b

--- src/abc148_b/index.js
+++ src/abc148_b/index.js
@@ -3,18 +3,27 @@ import * as fs from 'fs';
 import 'core-js';
 // import bigInt from 'big-integer'
 
+function sameLength(textS, textT) {
+  let texts = '';
+  for (let i = 0; i < textS.length; i++) {
+    texts = texts + textS[i] + textT[i];
+  }
+
+  console.log(texts);
+}
+
 // inputに入力データ全体が入る
 function main(input) {
   // 1行目がinput[0], 2行目がinput[1], …に入る
   // input[0]に' 'で分けられてる
-  const inputs = input.split('\n')[0].split(' ');
-  console.log(inputs);
+  // const inputs0 = input.split('\n')[0].split(' ');
+  const inputs1 = input.split('\n')[1].split(' ');
   //文字列から10進数に変換するときはparseIntを使います
-  const a1 = parseInt(inputs[0], 10);
-  const a2 = parseInt(inputs[1], 10);
-  const a3 = parseInt(inputs[2], 10);
+  // const lengthN = parseInt(inputs0[0], 10);
+  const textS = inputs1[0];
+  const textT = inputs1[1];
   //出力
-  console.log(a1, a2, a3);
+  sameLength(textS, textT);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
