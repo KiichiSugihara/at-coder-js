83be2075fe823e70c21cb7823a550473f539572a
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-10 21:49:33 +0900

    finish: abc150_a

---
+++ src/abc150_a/index.js
@@ -0,0 +1,22 @@
+'use strict';
+import * as fs from 'fs';
+import 'core-js';
+// import bigInt from 'big-integer'
+
+// inputに入力データ全体が入る
+function main(input) {
+  // 1行目がinput[0], 2行目がinput[1], …に入る
+  // input[0]に' 'で分けられてる
+  const inputs = input.split('\n')[0].split(' ');
+  //文字列から10進数に変換するときはparseIntを使います
+  const k = parseInt(inputs[0], 10);
+  const x = parseInt(inputs[1], 10);
+  const sum = k * 500;
+  if (sum < x) {
+    console.log('No');
+  } else {
+    console.log('Yes');
+  }
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
