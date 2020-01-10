c29d4cf95b39be2a317dac5d35412cb6f2aa47fc
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-10 21:52:34 +0900

    setup: abc150_c

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {};
 
-config.PROBLEM_URL = 'https://atcoder.jp/contests/abc148/tasks/abc148_d';
-config.PROBLEM_NAME = 'abc148_d';
+config.PROBLEM_URL = 'https://atcoder.jp/contests/abc150/tasks/abc150_c';
+config.PROBLEM_NAME = 'abc150_c';
 
 module.exports = config;
