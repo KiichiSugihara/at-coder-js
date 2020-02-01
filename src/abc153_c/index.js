755515ab262b748b31131f694f3b71b724635148
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-02-01 19:58:01 +0900

    rewrite: abc153_c

--- src/abc153_c/index.js
+++ src/abc153_c/index.js
@@ -1,24 +1,24 @@
 'use strict';
 import * as fs from 'fs';
 import 'core-js';
-import bigInt from 'big-integer';
 
 // inputに入力データ全体が入る
-function main(input) {
+function main(input_raw) {
+  const input = input_raw.split('\n');
   // 1行目がinput[0], 2行目がinput[1], …に入る
   // input[0]に' 'で分けられてる
-  const inputs0 = input.split('\n')[0].split(' ');
-  const monstersHP = input.split('\n')[1].split(' ');
-  //文字列から10進数に変換するときはparseIntを使います
-  const n = parseInt(inputs0[0], 10);
-  const skillCounts = parseInt(inputs0[1], 10);
+  const N = Number(input[0].split(' ')[0]);
+  const skillCounts = Number(input[0].split(' ')[1]);
+  const monstersHP = input[1].split(' ').map(Number);
   //出力
-  monstersHP.sort();
-  let sum = bigInt(0);
-  for (let i = 0; i < n - skillCounts; i++) {
-    sum = bigInt(sum).plus(monstersHP[i]);
+  const sortDesc = monstersHP.sort(function(a, b) {
+    return b - a;
+  });
+  let sum = 0;
+  for (let i = skillCounts; i < N; i++) {
+    sum += sortDesc[i];
   }
-  console.log(bigInt(sum).toString());
+  console.log(sum);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
